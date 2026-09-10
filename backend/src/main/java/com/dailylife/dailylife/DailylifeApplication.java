6aa71a9fffd19cc4da3a71a30309e72bc98f5230
dumpalashivanjali <dumpalashivanjali8@gmail.com>
2026-09-10 19:26:53 +0530

    completed: create project+github and project architecture, git, and maven

---
+++ backend/src/main/java/com/dailylife/dailylife/DailylifeApplication.java
@@ -0,0 +1,13 @@
+package com.dailylife.dailylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailylifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailylifeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dailylife/dailylife/DailylifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailylife.dailylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailylifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
